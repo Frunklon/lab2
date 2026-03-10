badf7e20c4e5ca3a4eec1d491a82149669c24a82
Maksym <maksym7123@gmail.com>
2026-03-10 13:04:25 +0100

    Dodanie nowych zmian

--- main.py
+++ main.py
@@ -7,6 +7,7 @@ import json
 class Parameters(BaseModel):
     apartments_json_path: str = 'data/apartments.json'
     tenants_json_path: str = 'data/tenants.json'
+    bills_json_path: str = 'data/bills.json'
 
 
 class Room(BaseModel):
@@ -72,4 +73,7 @@ if __name__ == '__main__':
 
     for tenant in manager.tenants.values():
         print(tenant.name, tenant.apartment, tenant.room, tenant.rent_pln, tenant.deposit_pln, tenant.date_agreement_from, tenant.date_agreement_to)
+    
+
+        
     
